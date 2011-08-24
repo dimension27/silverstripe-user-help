4d9c1db33b538bdaec5a19e585cb82022576c895
Simon Wade <simon.wade@gmail.com>
2011-08-25 01:31:47 +1000

    Added unset HTTP_HOST to sake.sh because cli-script.php complains if HTTP_HOST is set

--- script/sake.sh
+++ script/sake.sh
@@ -3,6 +3,7 @@
 # Runs sake as the webserver user
 
 source `dirname $0`/env.sh
+unset HTTP_HOST # cli-script.php complains if HTTP_HOST is set
 cd "$BASE_PATH/public"
 `getSudo` ./sapphire/sake $@
 echo
